2d016527fee83980b2536861ee142aad766c15ef
Elzoka <mahmoud_elzoka@yahoo.com>
2018-04-29 14:46:17 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, '/client/src/app.js'),
+    output: {
+        path: path.join(__dirname, '/client/public'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, '/client/public'),
+        historyApiFallback: true
+    }
+}
